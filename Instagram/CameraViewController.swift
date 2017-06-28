21be1dc794b5e197854478d1fdb92ce9c5a08b7e
Chase Warren <chasejjw@fb.com>
2017-06-27 17:16:42 -0700

    Added camera page and image picker

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBAction func onPick(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.camera
+        
+        self.present(vc, animated: true, completion: nil)
+        
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            // let size = CGSize(width: <#T##Int#>, height: <#T##Int#>)
+            // let resizedOriginalImage = resize(image: originalImage, newSize: )
+            // let resizedEditedImage = resize(image: originalImage, newSize: )
+            // Do something with the images (based on your use case)
+            
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+//    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+//        let resizeImageView = UIImageView(frame: CGRect(0, 0, newSize.width, newSize.height))
+//        resizeImageView.contentMode = UIViewContentMode.ScaleAspectFill
+//        resizeImageView.image = image
+//        
+//        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+//        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//        return newImage!
+//    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
