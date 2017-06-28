3dcf0830f9b729220954a651c6b8399a94d34367
Chase Warren <chasejjw@fb.com>
2017-06-28 13:43:44 -0700

    Added feed layout and posting

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -12,37 +12,44 @@ import Parse
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var captionField: UITextField!
-    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var imagePreview: UIImageView!
+
     
     var postCaption: String = ""
     
     @IBAction func onPost(_ sender: Any) {
         postCaption = captionField.text!
+        dismiss(animated: true, completion: nil)
+        Post.postUserImage(image: imagePreview.image, withCaption: postCaption) { (success: Bool, error: Error?) in
+            if success {
+                print("Image posted!")
+            } else {
+                print("Failed to post image")
+            }
+        }
     }
     
     @IBAction func onPick(_ sender: Any) {
         let vc = UIImagePickerController()
         vc.delegate = self
         vc.allowsEditing = true
-        vc.sourceType = UIImagePickerControllerSourceType.camera
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
         
         self.present(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-            
-            let image = info[UIImagePickerControllerEditedImage] as! UIImage
-            
-            postImage.image = image
-            
-            // let size = CGSize(width: <#T##Int#>, height: <#T##Int#>)
-            // let resizedOriginalImage = resize(image: originalImage, newSize: )
-            // let resizedEditedImage = resize(image: originalImage, newSize: )
-            // Do something with the images (based on your use case)
-            
-            
-            
-            dismiss(animated: true, completion: nil)
-        }
+        let userImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        imagePreview.image = userImage
+        
+        // let size = CGSize(width: <#T##Int#>, height: <#T##Int#>)
+        // let resizedOriginalImage = resize(image: originalImage, newSize: )
+        // let resizedEditedImage = resize(image: originalImage, newSize: )
+        // Do something with the images (based on your use case)
+        
+        dismiss(animated: true, completion: nil)
     }
     
 //    func resize(image: UIImage, newSize: CGSize) -> UIImage {
@@ -71,64 +78,5 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-
-class Post: NSObject {
-    /**
-     * Other methods
-     */
-    
-    /**
-     Method to add a user post to Parse (uploading image file)
-     
-     - parameter image: Image that the user wants upload to parse
-     - parameter caption: Caption text input by the user
-     - parameter completion: Block to be executed after save operation is complete
-     */
-    class func postUserImage(image: UIImage?, withCaption caption: String?, withCompletion completion: PFBooleanResultBlock?) {
-        
-        // Create Parse object PFObject
-        let post = PFObject(className: "Post")
-        
-        // Add relevant fields to the object
-        post["media"] = getPFFileFromImage(image: image) // PFFile column type
-        post["author"] = PFUser.current() // Pointer column type that points to PFUser
-        post["caption"] = caption
-        post["likesCount"] = 0
-        post["commentsCount"] = 0
-        
-        // Save object (following function will save the object in Parse asynchronously)
-        post.saveInBackground(block: completion)
-    }
-    
-    /**
-     Method to convert UIImage to PFFile
-     
-     - parameter image: Image that the user wants to upload to parse
-     
-     - returns: PFFile for the the data in the image
-     */
-    class func getPFFileFromImage(image: UIImage?) -> PFFile? {
-        // check if image is not nil
-        if let image = image {
-            // get image data and check if that is not nil
-            if let imageData = UIImagePNGRepresentation(image) {
-                return PFFile(name: "image.png", data: imageData)
-            }
-        }
-        return nil
     }
 }
