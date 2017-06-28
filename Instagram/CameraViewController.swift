704ef130019276780fc31c4500ef0f00d2f4dc37
Chase Warren <chasejjw@fb.com>
2017-06-27 17:49:40 -0700

    Added caption creation

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var captionField: UITextField!
+    
+    var postImage: UIImage
+    var postCaption: String = ""
+    
+    @IBAction func onPost(_ sender: Any) {
+        postCaption = captionField.text
+    }
+    
     @IBAction func onPick(_ sender: Any) {
         let vc = UIImagePickerController()
         vc.delegate = self
@@ -19,12 +29,15 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         self.present(vc, animated: true, completion: nil)
         
         func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-            let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            
+            let image = info[UIImagePickerControllerEditedImage] as! UIImage
+            
             // let size = CGSize(width: <#T##Int#>, height: <#T##Int#>)
             // let resizedOriginalImage = resize(image: originalImage, newSize: )
             // let resizedEditedImage = resize(image: originalImage, newSize: )
             // Do something with the images (based on your use case)
+            
+            
             
             dismiss(animated: true, completion: nil)
         }
@@ -69,4 +82,51 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     */
 
+}
+
+class Post: NSObject {
+    /**
+     * Other methods
+     */
+    
+    /**
+     Method to add a user post to Parse (uploading image file)
+     
+     - parameter image: Image that the user wants upload to parse
+     - parameter caption: Caption text input by the user
+     - parameter completion: Block to be executed after save operation is complete
+     */
+    class func postUserImage(image: UIImage?, withCaption caption: String?, withCompletion completion: PFBooleanResultBlock?) {
+        
+        // Create Parse object PFObject
+        let post = PFObject(className: "Post")
+        
+        // Add relevant fields to the object
+        post["media"] = getPFFileFromImage(image: image) // PFFile column type
+        post["author"] = PFUser.current() // Pointer column type that points to PFUser
+        post["caption"] = caption
+        post["likesCount"] = 0
+        post["commentsCount"] = 0
+        
+        // Save object (following function will save the object in Parse asynchronously)
+        post.saveInBackground(block: completion)
+    }
+    
+    /**
+     Method to convert UIImage to PFFile
+     
+     - parameter image: Image that the user wants to upload to parse
+     
+     - returns: PFFile for the the data in the image
+     */
+    class func getPFFileFromImage(image: UIImage?) -> PFFile? {
+        // check if image is not nil
+        if let image = image {
+            // get image data and check if that is not nil
+            if let imageData = UIImagePNGRepresentation(image) {
+                return PFFile(name: "image.png", data: imageData)
+            }
+        }
+        return nil
+    }
 }
